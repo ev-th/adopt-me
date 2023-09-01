0b9c193569d5eff4c0d1f5a23bc19c14df29807a
Evan Thomas <evanthomas0292@gmail.com>
2023-09-01 23:05:15 +0100

    Make useBreedList custom hook and extract Results from SearchParams

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
+import Results from "./Results";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -7,7 +9,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = ["Poodle"];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -75,14 +77,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };

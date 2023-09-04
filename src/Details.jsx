ab62f252f1ed2de0ce2eb1fde9d698ea7bd309e5
Evan Thomas <evanthomas0292@gmail.com>
2023-09-04 12:31:48 +0100

    Add Details component and use react query

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet)
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">Spinner</h2>
+      </div>
+    )
+  }
+
+  const pet = results.data.pets[0]
+
   return (
-    <div>Details</div>
+    <div className="details">
+      <h1>{pet.name}</h1>
+      <h2>{pet.animal} - {pet.breed} - {pet.city} - {pet.state}</h2>
+      <button>Adopt {pet.name}</button>
+      <p>{pet.description}</p>
+    </div>
   )
 }
 
